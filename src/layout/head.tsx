a491659f5cb485150793221ff85e0f73c2a8f181
evanfu <evanfu08@gmail.com>
2025-12-09 16:34:30 +0800

    Vercel Analytics
    Vercel Analytics

--- src/layout/head.tsx
+++ src/layout/head.tsx
@@ -1,4 +1,4 @@
-import Script from 'next/script'
+
 
 export default function Head() {
 	return (
@@ -13,16 +13,7 @@ export default function Head() {
 
 			<link href='https://fonts.googleapis.cn/css2?family=Averia+Gruesa+Libre&display=swap' rel='stylesheet' />
 
-			<Script src='https://www.googletagmanager.com/gtag/js?id=G-ZNSFR7C9PM' />
-			<Script id='google-analytics'>
-				{`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
 
-          gtag('config', 'G-ZNSFR7C9PM');
-        `}
-			</Script>
 		</head>
 	)
 }
